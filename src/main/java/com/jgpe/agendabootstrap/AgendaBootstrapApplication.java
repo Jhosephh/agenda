ccf3305ca8ddb3b7e0dc1ddc61ce2534c21185b0
Jhosephh <lup_1284@hotmail.com>
2023-01-02 14:09:24 -0600

    first commit

---
+++ src/main/java/com/jgpe/agendabootstrap/AgendaBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.jgpe.agendabootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgpe/agendabootstrap/AgendaBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgpe.agendabootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
